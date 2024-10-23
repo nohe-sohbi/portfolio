5dff270733e2258f67c86566c24d4e37c7065bed
Nohé <sohbi.nohe@gmail.com>
2024-10-24 01:01:17 +0200

    Modifying the way tiles are displayed

--- src/components/ProjectTile.tsx
+++ src/components/ProjectTile.tsx
@@ -3,16 +3,41 @@ import { Project } from "../projects/ProjectsData";
 export default function ProjectTile(data: Project) {
   return (
     <>
-      <div className="project-tile" onClick={() => window.open(data.links)}>
-        <h2>
-          {data.name} - {data.technologies}
-        </h2>
-
+      <div
+        className="project-tile"
+        onClick={() => data.links && window.open(data.links)}
+      >
+        <h2>{data.name}</h2>
+        <div className="technologies">
+          {Array.isArray(data.technologies) ? (
+            data.technologies.map((t: string) => <img src={t} width={30} />)
+          ) : (
+            <img src={data.technologies} />
+          )}
+        </div>
         <hr />
 
         <p>{data.description}</p>
 
-        <img src={data.image} alt={data.name} />
+        {data.image && (
+          <img
+            src={data.image}
+            alt={data.name}
+            style={{
+              objectFit: "cover",
+              width: 300,
+              height: 300,
+              borderRadius: 10,
+              border: "1px solid #995d25",
+              cursor: "pointer",
+              transition: "all 0.2s ease-in-out",
+              textAlign: "center",
+              display: "block",
+              margin: "0 auto",
+              marginBottom: "10px",
+            }}
+          />
+        )}
       </div>
     </>
   );
