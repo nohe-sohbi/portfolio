517e7901451edefcfb67a42194610122050cf564
Nohé <sohbi.nohe@gmail.com>
2024-10-23 21:46:05 +0200

    Added Header Component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <>
+      <header>
+        <title>Portfolio</title>
+        <nav>
+          <a href="mailto:nohe@sohbi.dev">nohe@sohbi.dev</a>
+        </nav>
+      </header>
+    </>
+  );
+}
