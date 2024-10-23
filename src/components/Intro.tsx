f7a3708879b48345fb7e8e79b366a75733ae63e6
Nohé <sohbi.nohe@gmail.com>
2024-10-24 01:01:40 +0200

    Modifying Intro Text

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,13 +2,23 @@ export default function Intro() {
   return (
     <>
       <div>
-        <h1>Welcome to my portfolio!</h1>
+        <h1
+          style={{
+            fontSize: "4rem",
+            fontWeight: 700,
+            marginBottom: "1rem",
+            backgroundImage:
+              "linear-gradient(135deg, #f0f0f0 0%, #e0993c 100%)",
+            WebkitBackgroundClip: "text",
+            color: "transparent",
+          }}
+        >
+          Portfolio
+        </h1>
         <p>
           Hello 😉, you can see some of my projects listed here, please contact
-          me for any query i will be happy to discuss with you !
+          me for any query. I would be thrilled to discuss anything with you!
         </p>
-
-        <hr />
       </div>
     </>
   );
