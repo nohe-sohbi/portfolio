376555a5ff0fc54a9d60502306592b1f11e6f9cc
Nohé <sohbi.nohe@gmail.com>
2024-10-23 21:46:13 +0200

    Added footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <p>
+          Built with{" "}
+          <a href="https://vitejs.dev" target="_blank">
+            Vite
+          </a>{" "}
+          and{" "}
+          <a href="https://reactjs.org" target="_blank">
+            React
+          </a>
+        </p>
+      </footer>
+    </>
+  );
+}
