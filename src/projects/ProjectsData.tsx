6773ec2091d7277618efd84cb1e3c030eed84083
Nohé <sohbi.nohe@gmail.com>
2024-10-24 01:00:58 +0200

    Adding project list details

--- src/projects/ProjectsData.tsx
+++ src/projects/ProjectsData.tsx
@@ -1,53 +1,91 @@
 export type Project = {
   name: string;
   description: string;
-  technologies: string;
-  links: string;
+  technologies?: string | string[];
+  links?: string;
   image?: string;
+  size?: string;
 };
 
 const ProjectsData: Project[] = [
   {
-    name: "Project 1",
-    description: "Project 1 description",
-    technologies: "Project 1 technologies",
-    links: "Project 1 links",
-    image: "https://picsum.photos/300",
+    name: "Go Techniques",
+    description:
+      "Website who can generate quotes based on the clients request for Geotechnical services.",
+    technologies: [
+      "src/assets/technologies/PHP.png",
+      "src/assets/technologies/MySQL.png",
+      "src/assets/technologies/HTML5.png",
+      "src/assets/technologies/CSS3.png",
+    ],
+    links: "https://gotechniques.fr",
+    image: "src/assets/websites/gotechniques.fr.png",
+    size: "100%",
   },
   {
-    name: "Project 2",
-    description: "Project 2 description",
-    technologies: "Project 2 technologies",
+    name: "Fuzion",
+    description:
+      "Web builder for making an executable binded into another executable with custom parameters.",
+    technologies: [
+      "src/assets/technologies/PHP.png",
+      "src/assets/technologies/CSHARP.png",
+      "src/assets/technologies/HTML5.png",
+      "src/assets/technologies/CSS3.png",
+    ],
     links: "Project 2 links",
-    image: "https://picsum.photos/200",
+    image: "src/assets/websites/crypter.png",
+    size: "100%",
   },
   {
-    name: "Project 3",
-    description: "Project 3 description",
-    technologies: "Project 3 technologies",
+    name: "AsyncNode",
+    description:
+      "Single page website to provide custom web developpement prestations for customers.",
+    technologies: [
+      "src/assets/technologies/PHP.png",
+      "src/assets/technologies/HTML5.png",
+      "src/assets/technologies/CSS3.png",
+    ],
     links: "Project 3 links",
-    image: "https://picsum.photos/200",
+    image: "src/assets/websites/asyncnode.png",
+    size: "100%",
   },
   {
-    name: "Project 4",
-    description: "Project 4 description",
-    technologies: "Project 4 technologies",
+    name: "Mon petit cadeau",
+    description:
+      "Custom project to provide guessing game to customers who doesn't have gift ideas.",
+    technologies: [
+      "src/assets/technologies/PHP.png",
+      "src/assets/technologies/MySQL.png",
+      "src/assets/technologies/HTML5.png",
+      "src/assets/technologies/CSS3.png",
+    ],
     links: "Project 4 links",
-    image: "https://picsum.photos/200",
+    image: "src/assets/websites/mon-petit-cadeau.png",
   },
   {
-    name: "Project 5",
-    description: "Project 5 description",
-    technologies: "Project 5 technologies",
+    name: "SNAgency",
+    description:
+      "SMMA agency website specialised in google ads management campaigns.",
+    technologies: [
+      "src/assets/technologies/PHP.png",
+      "src/assets/technologies/MySQL.png",
+      "src/assets/technologies/HTML5.png",
+      "src/assets/technologies/CSS3.png",
+    ],
     links: "Project 5 links",
-    image: "https://picsum.photos/200",
+    image: "src/assets/websites/snagency.png",
   },
   {
-    name: "Project 6",
-    description: "Project 6 description",
-    technologies: "Project 6 technologies",
-    links: "Project 6 links",
-    image: "https://picsum.photos/200",
+    name: "Elite-Cheaters",
+    description:
+      "Aside project to sell custom cheating softwares for different games.",
+    technologies: [
+      "src/assets/technologies/PHP.png",
+      "src/assets/technologies/MySQL.png",
+      "src/assets/technologies/HTML5.png",
+      "src/assets/technologies/CSS3.png",
+    ],
+    image: "src/assets/websites/elite-cheaters.png",
   },
 ];
 
