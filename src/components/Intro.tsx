17582517f47f256304df8a56c0e5d4a2f3a643b4
Nohé <sohbi.nohe@gmail.com>
2024-10-23 21:45:57 +0200

    Adding intro component

---
+++ src/components/Intro.tsx
@@ -0,0 +1,15 @@
+export default function Intro() {
+  return (
+    <>
+      <div>
+        <h1>Welcome to my portfolio!</h1>
+        <p>
+          Hello 😉, you can see some of my projects listed here, please contact
+          me for any query i will be happy to discuss with you !
+        </p>
+
+        <hr />
+      </div>
+    </>
+  );
+}
