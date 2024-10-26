1009ea58edf5387c911abbfd287af00d38afc059
Nohé <sohbi.nohe@gmail.com>
2024-10-26 22:03:12 +0200

    Changing visual presentation for frontpage, added 2 images bottom left&right, and changed background color

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,19 @@ function App() {
     <>
       <div id="main-app">
         <div className="smooth-scroll">
+          <img
+            src="src/assets/pc.png"
+            alt="Top Right Image"
+            className="image-top-right"
+          />
           <Header />
           <Intro />
           <Portfolio projects={ProjectsData} />
+          <img
+            src="src/assets/clavier.png"
+            alt="Bottom Left Image"
+            className="image-bottom-left"
+          />
           <Footer />
         </div>
       </div>
